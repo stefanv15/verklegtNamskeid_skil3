bf79c1e36365744b9499b96358db3792b3880705
Eyþór Traustason <Eyr Traustason>
2015-12-11 14:19:38 +0000

    sælirnu fallegt forrit. hæ stebbo

--- main.cpp
+++ main.cpp
@@ -7,13 +7,13 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     QSplashScreen *splash = new QSplashScreen;
-    splash -> setPixmap(QPixmap("C:/Users/Eyþór/Pictures/stef.jpg"));
+    splash -> setPixmap(QPixmap("stef.jpg"));
     splash->show();
 
     MainWindow window;
 
-    QTimer::singleShot(3000, splash, SLOT(close()));
-    QTimer::singleShot(3000, &window, SLOT(show()));
+    QTimer::singleShot(2000, splash, SLOT(close()));
+    QTimer::singleShot(2000, &window, SLOT(show()));
     //window.show();
 
     return a.exec();
